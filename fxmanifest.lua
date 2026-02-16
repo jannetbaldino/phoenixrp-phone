a0aadb221706080aef6e5a7e4632fcf12c4501d5
jannetbaldino <jannetbaldino@gmail.com>
2026-02-15 23:13:36 -0500

    Update fxmanifest
    Changed UI files to shared UI

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,10 +6,12 @@ lua54 'yes'
 description 'PRP Phone (NUI)'
 version '0.0.2'
 
-ui_page 'ui/index.html'
+ui_page 'ui/dist/index.html'
 
 files {
   'ui/index.html',
+  'ui/dist/index.html',
+  'ui/dist/assets/**',
   'ui/styles.css',
   'ui/app.js',
   'ui/assets/*'
